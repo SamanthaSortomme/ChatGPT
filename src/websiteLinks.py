57b13689a56efe020dc9bbd4dd24366494a77159
SamanthaSortomme <sor23001@byui.edu>
2023-05-16 10:46:47 -0700

    All typed out, still has errors

---
+++ src/websiteLinks.py
@@ -0,0 +1,15 @@
+
+# plus sign
+# https://www.merriam-webster.com/dictionary/plus%20sign
+
+# font sans
+# https://fonts.google.com/specimen/Open+Sans?query=open+
+
+# weird arrow
+# https://www.compart.com/en/unicode/U+27A2
+
+# 141 install packages, don't forget
+
+# 148 chat completion
+# https://platform.openai.com/docs/api-reference/chat
+
